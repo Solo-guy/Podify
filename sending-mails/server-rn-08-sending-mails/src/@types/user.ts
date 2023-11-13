e235062e835b287ae04d1846429be6ba242f29c0
front end wizzard <101088369+Solo-guy@users.noreply.github.com>
2023-11-13 12:14:13 +0700

    day 5

--- sending-mails/server-rn-08-sending-mails/src/@types/user.ts
+++
@@ -1,9 +0,0 @@
-import { Request } from "express";
-
-export interface CreateUser extends Request {
-  body: {
-    name: string;
-    email: string;
-    password: string;
-  };
-}
--- sending-mails/server-rn-08-sending-mails/src/utils/variables.ts
+++
@@ -1,3 +0,0 @@
-const { env } = process as { env: { [key: string]: string } };
-
-export const { MONGO_URI, MAILTRAP_USER, MAILTRAP_PASS } = env;
