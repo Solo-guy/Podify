57ee86a870fbcc2a05ba9682bdd7e17e58ea8ca9
front end wizzard <101088369+Solo-guy@users.noreply.github.com>
2023-11-26 10:51:52 +0700

    day 16

--- server/src/routers/favorite.ts
+++ server/src/routers/favorite.ts
@@ -1,9 +1,15 @@
-import { toggleFavorite } from "#/controllers/favorite";
+import {
+  getFavorites,
+  getIsFavorites,
+  toggleFavorite,
+} from "#/controllers/favorite";
 import { isVerified, mustAuth } from "#/middleware/auth";
 import { Router } from "express";
 
 const router = Router();
 
 router.post("/", mustAuth, isVerified, toggleFavorite);
+router.get("/", mustAuth, getFavorites);
+router.get("/is-fav", mustAuth, getIsFavorites);
 
 export default router;
