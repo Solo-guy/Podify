0d7d0152e1c0e9f572cf7f116c4a2f2a5ddfe381
front end wizzard <101088369+Solo-guy@users.noreply.github.com>
2023-11-27 11:16:33 +0700

    day 17

--- server/src/utils/validationSchema.ts
+++ server/src/utils/validationSchema.ts
@@ -66,6 +66,17 @@ export const AudioValidationSchema = yup.object().shape({
   about: yup.string().required("About is missing!"),
   category: yup
     .string()
-    .oneOf(categories, "Invalid category")
+    .oneOf(categories, "Invalid category!")
     .required("Category is missing!"),
+});
+
+export const NewPlaylistValidationSchema = yup.object().shape({
+  title: yup.string().required("Title is missing!"),
+  resId: yup.string().transform(function (value) {
+    return this.isType(value) && isValidObjectId(value) ? value : "";
+  }),
+  visibility: yup
+    .string()
+    .oneOf(["public", "private"], "Visibility must be public or private!")
+    .required("Visibility is missing!"),
 });
