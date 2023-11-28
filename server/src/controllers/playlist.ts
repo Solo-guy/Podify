f5a8a8baa596412b76486c22a7d44b0d18445b9f
front end wizzard <101088369+Solo-guy@users.noreply.github.com>
2023-11-28 12:05:12 +0700

    day 18

--- server/src/controllers/playlist.ts
+++ server/src/controllers/playlist.ts
@@ -1,7 +1,12 @@
-import { CreatePlaylistRequest } from "#/@types/audio";
+import {
+  CreatePlaylistRequest,
+  PopulateFavList,
+  UpdatePlaylistRequest,
+} from "#/@types/audio";
 import Audio from "#/models/audio";
 import Playlist from "#/models/playlist";
 import { RequestHandler } from "express";
+import { isValidObjectId } from "mongoose";
 
 export const createPlaylist: RequestHandler = async (
   req: CreatePlaylistRequest,
@@ -34,6 +39,142 @@ export const createPlaylist: RequestHandler = async (
       id: newPlaylist._id,
       title: newPlaylist.title,
       visibility: newPlaylist.visibility,
+    },
+  });
+};
+
+export const updatePlaylist: RequestHandler = async (
+  req: UpdatePlaylistRequest,
+  res
+) => {
+  const { id, item, title, visibility } = req.body;
+
+  const playlist = await Playlist.findOneAndUpdate(
+    { _id: id, owner: req.user.id },
+    { title, visibility },
+    { new: true }
+  );
+
+  if (!playlist) return res.status(404).json({ error: "Playlist not found !" });
+
+  if (item) {
+    const audio = await Audio.findById(item);
+    if (!audio) return res.status(404).json({ error: "Audio not found !" });
+    // playlist.items.push(audio._id);
+    // await playlist.save();
+
+    await Playlist.findByIdAndUpdate(playlist._id, {
+      $addToSet: { items: item },
+    });
+  }
+
+  res.status(201).json({
+    playlist: {
+      id: playlist._id,
+      title: playlist.title,
+      visibility: playlist.visibility,
+    },
+  });
+};
+
+export const removePlaylist: RequestHandler = async (req, res) => {
+  const { playlistId, resId, all } = req.query;
+
+  if (!isValidObjectId(playlistId))
+    return res.status(422).json({ error: "Invalid playlist id!" });
+
+  if (all === "yes") {
+    const playlist = await Playlist.findOneAndDelete({
+      _id: playlistId,
+      owner: req.user.id,
+    });
+
+    if (!playlist)
+      return res.status(404).json({ error: "Playlist not found!" });
+  }
+
+  if (resId) {
+    if (!isValidObjectId(resId))
+      return res.status(422).json({ error: "Invalid audio id!" });
+
+    const playlist = await Playlist.findOneAndUpdate(
+      {
+        _id: playlistId,
+        owner: req.user.id,
+      },
+      {
+        $pull: { items: resId },
+      }
+    );
+
+    if (!playlist)
+      return res.status(404).json({ error: "Playlist not found!" });
+  }
+
+  res.json({ success: true });
+};
+
+export const getPlaylistByProfile: RequestHandler = async (req, res) => {
+  const { pageNo = "0", limit = "20" } = req.query as {
+    pageNo: string;
+    limit: string;
+  };
+
+  const data = await Playlist.find({
+    owner: req.user.id,
+    visibility: { $ne: "auto" },
+  })
+    .skip(parseInt(pageNo) * parseInt(limit))
+    .limit(parseInt(limit))
+    .sort("-createdAt");
+
+  const playlist = data.map((item) => {
+    return {
+      id: item._id,
+      title: item.title,
+      itemsCount: item.items.length,
+      visibility: item.visibility,
+    };
+  });
+
+  res.json({ playlist });
+};
+
+export const getAudios: RequestHandler = async (req, res) => {
+  const { playlistId } = req.params;
+
+  if (!isValidObjectId(playlistId))
+    return res.status(422).json({ error: "Invalid playlist id!" });
+
+  const playlist = await Playlist.findOne({
+    owner: req.user.id,
+    _id: playlistId,
+  }).populate<{ items: PopulateFavList[] }>({
+    path: "items",
+    populate: {
+      path: "owner",
+      select: "name",
+    },
+  });
+
+  if (!playlist) return res.json({ list: [] });
+
+  const audios = playlist.items.map((item) => {
+    return {
+      id: item._id,
+      title: item.title,
+      category: item.category,
+      file: item.file.url,
+      poster: item.poster?.url,
+      owner: { name: item.owner.name, id: item.owner._id },
+    };
+  });
+
+  res.json({
+    list: {
+      id: playlist._id,
+      title: playlist.title,
+      audios,
     },
   });
 };
--- server/src/routers/playlist.ts
+++ server/src/routers/playlist.ts
@@ -1,7 +1,16 @@
-import { createPlaylist } from "#/controllers/playlist";
+import {
+  createPlaylist,
+  getAudios,
+  getPlaylistByProfile,
+  removePlaylist,
+  updatePlaylist,
+} from "#/controllers/playlist";
 import { isVerified, mustAuth } from "#/middleware/auth";
 import { validate } from "#/middleware/validator";
-import { NewPlaylistValidationSchema } from "#/utils/validationSchema";
+import {
+  NewPlaylistValidationSchema,
+  OldPlaylistValidationSchema,
+} from "#/utils/validationSchema";
 import { Router } from "express";
 
 const router = Router();
@@ -13,5 +22,17 @@ router.post(
   validate(NewPlaylistValidationSchema),
   createPlaylist
 );
+
+router.patch(
+  "/",
+  mustAuth,
+  validate(OldPlaylistValidationSchema),
+  updatePlaylist
+);
+
+router.delete("/", mustAuth, removePlaylist);
+
+router.get("/by-profile", mustAuth, getPlaylistByProfile);
+router.get("/:playlistId", mustAuth, getAudios);
 
 export default router;
