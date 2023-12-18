d03bb6f33acd28182528385414c8f47ad88e2cae
front end wizzard <101088369+Solo-guy@users.noreply.github.com>
2023-12-18 19:40:13 +0700

    day 14

--- PodifyPlayer/App.tsx
+++ PodifyPlayer/App.tsx
@@ -1,14 +1,19 @@
 import AppCotnainer from '@components/AppCotnainer';
+import {QueryClient, QueryClientProvider} from 'react-query';
 import {Provider} from 'react-redux';
 import AppNavigator from 'src/navigation';
 import store from 'src/store';
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
     <Provider store={store}>
-      <AppCotnainer>
-        <AppNavigator />
-      </AppCotnainer>
+      <QueryClientProvider client={queryClient}>
+        <AppCotnainer>
+          <AppNavigator />
+        </AppCotnainer>
+      </QueryClientProvider>
     </Provider>
   );
 };
--- PodifyPlayer/src/views/Home.tsx
+++ PodifyPlayer/src/views/Home.tsx
@@ -1,18 +1,23 @@
+import LatestUploads from '@components/LatestUploads';
+import RecommendedAudios from '@components/RecommendedAudios';
 import {FC} from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 
 interface Props {}
 
 const Home: FC<Props> = props => {
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <LatestUploads />
+      <RecommendedAudios />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    padding: 10,
+  },
 });
 
 export default Home;
