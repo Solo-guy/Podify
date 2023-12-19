dda985f0e588646a0f683fffe91278232401cfd8
front end wizzard <101088369+Solo-guy@users.noreply.github.com>
2023-12-19 11:44:20 +0700

    day 15

--- PodifyPlayer/src/hooks/query.ts
+++ PodifyPlayer/src/hooks/query.ts
@@ -1,11 +1,14 @@
+import {getFromAsyncStorage, Keys} from '@utils/asyncStorage';
 import {useQuery} from 'react-query';
 import {useDispatch} from 'react-redux';
 import {AudioData} from 'src/@types/audio';
 import catchAsyncError from 'src/api/catchError';
-import client from 'src/api/client';
+import {getClient} from 'src/api/client';
 import {upldateNotification} from 'src/store/notification';
+import {Playlist} from 'src/@types/audio';
 
 const fetchLatest = async (): Promise<AudioData[]> => {
+  const client = await getClient();
   const {data} = await client('/audio/latest');
   return data.audios;
 };
@@ -22,6 +25,7 @@ export const useFetchLatestAudios = () => {
 };
 
 const fetchRecommended = async (): Promise<AudioData[]> => {
+  const client = await getClient();
   const {data} = await client('/profile/recommended');
   return data.audios;
 };
@@ -30,6 +34,28 @@ export const useFetchRecommendedAudios = () => {
   const dispatch = useDispatch();
   return useQuery(['recommended'], {
     queryFn: () => fetchRecommended(),
+    onError(err) {
+      const errorMessage = catchAsyncError(err);
+      dispatch(upldateNotification({message: errorMessage, type: 'error'}));
+    },
+  });
+};
+
+const fetchPlaylist = async (): Promise<Playlist[]> => {
+  const client = await getClient();
+  const token = await getFromAsyncStorage(Keys.AUTH_TOKEN);
+  const {data} = await client('/playlist/by-profile', {
+    headers: {
+      Authorization: 'Bearer ' + token,
+    },
+  });
+  return data.playlist;
+};
+
+export const useFetchPlaylist = () => {
+  const dispatch = useDispatch();
+  return useQuery(['playlist'], {
+    queryFn: () => fetchPlaylist(),
     onError(err) {
       const errorMessage = catchAsyncError(err);
       dispatch(upldateNotification({message: errorMessage, type: 'error'}));
