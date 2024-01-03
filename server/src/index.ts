2791880d44627f3daf1adae40a09d1e64d4ca9fa
front end wizzard <101088369+Solo-guy@users.noreply.github.com>
2024-01-03 18:09:32 +0700

    added home home route and not found

--- server/src/index.ts
+++ server/src/index.ts
@@ -30,6 +30,18 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 8989;
 
+app.get("/", (req, res) => {
+  res.json({
+    alive: true,
+  });
+});
+
+app.get("*", (req, res) => {
+  res.status(404).json({
+    error: "Not Found!",
+  });
+});
+
 app.listen(PORT, () => {
   console.log("Port is listening on port " + PORT);
 });
